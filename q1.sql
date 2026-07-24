d72fe8a4393645c5d6c6f8cf6282be549b5ebf85
ZahraSemsari <semsarizahra64@gmail.com>
2026-07-24 12:16:15 +0330

    Rewrite the third code in one statement with the same logic using with recursive

--- q1.sql
+++ q1.sql
@@ -4,48 +4,42 @@ CREATE INDEX index_sum ON orders (created_at , total);
 -- Section2 
 CREATE INDEX index_sum_two_condition ON orders (user_id , created_at , total  )
 -- Section3
-CREATE TEMPORARY TABLE daily_orders AS
-SELECT
-    DATE(created_at) AS order_date,
-    SUM(total) AS total_amount
-FROM orders
-GROUP BY DATE(created_at);
-CREATE TEMPORARY TABLE all_dates (
-    order_date DATE,
-    total_amount DECIMAL(10, 2)
-);
-DELIMITER //
-CREATE PROCEDURE have_all_date()
-BEGIN
-    DECLARE first_day DATE;
-    DECLARE last_day DATE;
-    SELECT
-        MIN(order_date),
-        MAX(order_date)
-    INTO
-        first_day,
-        last_day
-    FROM daily_orders;
-    WHILE first_day <= last_day DO
-        INSERT INTO all_dates (order_date, total_amount)
-        VALUES (first_day, 0);
-        SET first_day = DATE_ADD(first_day, INTERVAL 1 DAY);
-    END WHILE;
-END //
-DELIMITER ;
-CALL have_all_date();
-SELECT
-    order_date,
-    SUM(total_amount) AS total_amount
-FROM
-(
+WITH RECURSIVE
+
+daily_orders AS (
+    SELECT DATE(created_at) AS order_date, SUM(total) AS total_amount FROM orders
+    GROUP BY DATE(created_at)
+),
+
+date_boundry AS (
+    SELECT MIN(order_date) AS first_day, MAX(order_date) AS last_day
+    FROM daily_orders
+),
+
+all_dates AS (
+    SELECT first_day AS order_date, CAST(0 AS DECIMAL(10, 2)) AS total_amount, last_day
+    FROM date_boundry
+    WHERE first_day IS NOT NULL
+
+    UNION ALL
+
+    SELECT DATE_ADD(order_date, INTERVAL 1 DAY), CAST(0 AS DECIMAL(10, 2)), last_day
+    FROM all_dates
+    WHERE order_date < last_day
+),
+
+have_all_date AS (
     SELECT order_date, total_amount
     FROM all_dates
+
     UNION ALL
+
     SELECT order_date, total_amount
     FROM daily_orders
-) AS result
+)
+
+SELECT
+    order_date, SUM(total_amount) AS total_amount
+FROM have_all_date
 GROUP BY order_date
 ORDER BY order_date ASC;
-
-DROP PROCEDURE have_all_date;
